2748ea7ee435252215f4500cfbba7f34c6f53b39
SMART-TN-038 <blue672@naver.com>
2022-02-04 16:01:25 +0900

    Initial commit

---
+++ src/main/java/com/example/dockertest/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.dockertest.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping("/")
+    public ResponseEntity<?> getTest(){
+        return new ResponseEntity<String>("안녕",HttpStatus.OK);
+    }
+}
